596c4fa3d433a745fa4d99b7f633daad8d9f810c
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2020-07-22 18:26:53 +0100

    Added New and Create Post actions, added validtion for Posts and Users

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,3 +12,6 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - User and Post models and migrations.
 - Posts scaffold.
 - Basic authentication.
+- New and Create Actions
+- Validation for Posts and Users
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :username, presence: true, length: { maximum: 20 }, uniqueness: true
+  validates :email, presence: true, length: { minimum: 5 }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
